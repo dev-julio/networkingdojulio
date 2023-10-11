02672abffbdb59d641cd322aa87c1fda9e66ddba
Julio Cesar <101190914+dev-julio@users.noreply.github.com>
2023-10-11 19:51:16 -0300

    forms.py

--- networkingdojulio/forms.py
+++ networkingdojulio/forms.py
@@ -30,12 +30,12 @@ class FormEditarPerfil(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png'])])
 
-    curso_excel = BooleanField('Excel Impressionador')
-    curso_vba = BooleanField('VBA Impressionador')
-    curso_powerbi = BooleanField('Power BI Impressionador')
-    curso_python = BooleanField('Python Impressionador')
-    curso_ppt = BooleanField('Apresentações Impressionadoras')
-    curso_sql = BooleanField('SQL Impressionador')
+    curso_excel = BooleanField('Excel')
+    curso_vba = BooleanField('Dança')
+    curso_powerbi = BooleanField('Power BI')
+    curso_python = BooleanField('Python')
+    curso_ppt = BooleanField('Cantar')
+    curso_sql = BooleanField('Cozinhar')
     botao_submit_editarperfil = SubmitField('Confirmar Edição')
 
     def validate_email(self, email):
